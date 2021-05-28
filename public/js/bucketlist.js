39e46312d75ae7c40d0219cedd7145f8811e738c
usilwal <usilwal@hotmail.com>
2021-05-28 14:34:01 -0400

    deploy settings

--- public/js/bucketlist.js
+++ public/js/bucketlist.js
@@ -4,7 +4,7 @@ let loading = false;
 
 const getData = async () => {
     loading = true; 
-    const res = await fetch('http://localhost:3001/api/bucketlist')
+    const res = await fetch('http://localhost:7283/api/bucketlist')
     let data = await res.json();
     loading = false;
     console.log(data)
