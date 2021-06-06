9214c915f69083c0c2a70e2b4719c4f4721390a8
usilwal <usilwal@hotmail.com>
2021-06-05 23:52:49 -0400

    added mobile responsiveness to menu bar

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,12 @@
+function responsiveIcon() {
+    var x = document.getElementById("nav-link");
+    if (x.className === "nav-link") {
+      x.className += " responsive";
+    } else {
+      x.className = "nav-link";
+    }
+  }
+  
 $.get("html/navbar.html", function(data){
     $("#nav-placeholder").replaceWith(data);
 });
--- public/js/projectstimeline.js
+++ public/js/projectstimeline.js
@@ -42,6 +42,15 @@ const addData = async () => {
 
 addData()
 
+function responsiveIcon() {
+    var x = document.getElementById("nav-link");
+    if (x.className === "nav-link") {
+      x.className += " responsive";
+    } else {
+      x.className = "nav-link";
+    }
+  }
+  
 $.get("html/navbar.html", function(data){
     $("#nav-placeholder").replaceWith(data);
 });
